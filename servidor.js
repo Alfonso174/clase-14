cfae42bcf0d50bfb97af82a7f1ee6fd469ca6765
Alfonso <alfonsocor@gmail.com>
2019-06-06 20:54:04 -0500

    mas y mas

---
+++ servidor.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.get('/', function (req,res) {res.send('hola mundo')
+})
+
+app.listen(port,function() {
+    console.log(`Example app listening on port ${port}!`)
+})
